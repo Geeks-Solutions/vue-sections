d3bcadfd7d49e5c9ff45f9610b1f36617ffb97f6
Hussein Mahfouz <h.mahfouz@eweev.com>
2022-04-18 12:55:55 +0300

    Update the library of section front to have it use media · Issue #16 · Geeks-Solutions/vue-sections

--- src/configs/forms/wysiwyg.vue
+++ src/configs/forms/wysiwyg.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="input">
-    <quill-editor class="wyzywig" v-model="settings" />
+    <quill-editor ref="myQuillEditor" class="wyzywig" v-model="settings" />
   </div>
 </template>
 <script>
@@ -9,6 +9,7 @@ import "quill/dist/quill.snow.css";
 import "quill/dist/quill.bubble.css";
 
 import { quillEditor } from "vue-quill-editor";
+import axios from "axios";
 
 export default {
   components: {
@@ -16,12 +17,61 @@ export default {
   },
   data() {
     return {
-      settings: "",
+      settings: ""
     };
+  },
+  mounted() {
+    this.$refs.myQuillEditor.quill.getModule('toolbar').addHandler('image', () => {
+      this.uploadFunction();
+    });
   },
   methods: {
     validate() {
       return true;
+    },
+    uploadFunction() {
+      const input = document.createElement('input');
+      input.setAttribute('type', 'file');
+      input.click();
+
+      let imageURL = ''
+
+      // Listen upload local image and save to server
+      input.onchange = async () => {
+        const file = input.files[0];
+        const data = new FormData()
+        data.append('files[1][file]', file)
+        data.append('type', "image")
+        data.append('author', "Author")
+        data.append('private_status', "public")
+        data.append('files[1][platform_id]', '1')
+        const token = this.$cookies.get("sections-auth-token")
+          const config = {
+            headers: this.sectionHeader({token}),
+          };
+          await axios.post(
+              this.$sections.serverUrl +
+              `/project/${this.$sections.projectId}/media`,
+              // Uncomment the below line to overpass CORS
+              '/media/',
+              data,
+              config
+          ).then((result) => {
+            imageURL = result.data.files[0].url
+            const range = this.$refs.myQuillEditor.quill.getSelection();
+            this.$refs.myQuillEditor.quill.insertEmbed(range.index, 'image', imageURL);
+          })
+      };
+    },
+    sectionHeader(header) {
+      const timestamp = new Date().getTime();
+      const random = Math.floor(Math.random() * 1000000 + 1);
+      const header_key = `project-id-${timestamp}-${random}`;
+      header[header_key] = "a3b2cd1";
+      if(header.origin){
+        header['access-control-request-headers'] = header_key;
+      }
+      return header
     }
   }
 };
